a2d9305abe097d3bbd26063ea58472f4c0cf21e3
Raissa Ferreira <rai200890@gmail.com>
2018-10-29 00:10:09 -0300

    fix: Fix package import errors

--- test/test_hooks.py
+++ test/test_hooks.py
@@ -1,6 +1,6 @@
 from flask import g
 
-import flask_google_cloud_logger
+from flask_google_cloud_logger import hooks
 
 
 def test_flask_google_cloud_logger_before_request(app_client, mocker):
@@ -8,7 +8,7 @@ def test_flask_google_cloud_logger_before_request(app_client, mocker):
         "flask_google_cloud_logger.hooks.time",
         return_value=1540174450.9542747)
     with app_client.application.app_context():
-        flask_google_cloud_logger.flask_google_cloud_logger_before_request()
+        hooks.flask_google_cloud_logger_before_request()
         assert g.get("start_request") == 1540174450.9542747
 
 
@@ -19,7 +19,6 @@ def test_flask_google_cloud_logger_after_request(app_client, mocker):
             "flask_google_cloud_logger.hooks.time",
             return_value=1540175117.2246253)
         g.start_request = 1540174450.9542747
-        flask_google_cloud_logger.flask_google_cloud_logger_after_request(
-            response)
+        hooks.flask_google_cloud_logger_after_request(response)
         assert g.get("request_time") == 666270.3506946564
         assert g.response == response
