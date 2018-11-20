0bad01db9a819451dfd147458f3b0e9a25f4edc6
Raissa Ferreira <rai200890@gmail.com>
2018-11-20 16:16:37 -0200

    chore: Add tasks to automate package build process (#3)

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-__VERSION__ = "0.1.1"
+with open("README.md", "r") as output:
+    long_description = output.read()
+
+
+__VERSION__ = "0.1.2"
 
 setup(
     name="flask_google_cloud_logger",
     version=__VERSION__,
     description="Google Cloud Log Formatter for Flask",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="http://github.com/rai200890/flask_google_cloud_logger",
     author="Raissa Ferreira",
     author_email="rai200890@gmail.com",
     license="MIT",
-    packages=["flask_google_cloud_logger"],
+    packages=find_packages(),
     install_requires=[
-        "google_cloud_logger>=0.1.0",
+        "google_cloud_logger>=0.1.1",
         "flask>=1.0",
     ],
     classifiers=[
